a3fc76c5aaecd6d22733a97e67837222602f8219
Elvett <egor.priymak.2005@gmail.com>
2025-05-04 15:54:04 +0300

    lab4

---
+++ my_Ex2_OR.v
@@ -0,0 +1,16 @@
+module My_Ex2_OR(x1,x2,x3,f,f2);
+input x1,x2,x3;
+output f, f2;
+assign f = ~(
+   ~( x3  & ~x2 &  x1 ) &
+    ~(~x3  &  x2 &  x1 ) &
+    ~(~x3  & ~x2 & ~x1 )
+);
+assign f2 = (
+   ~( ~x3  &  ~x2 & x1 ) &
+   ~(x2 & x1 ) &
+   ~(x3 &  ~x1 ) &
+   ~(x2 &  ~x1 )
+);
+
+endmodule
